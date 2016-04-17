a7799d4d5b8498561826137fc02d8f3ecdf25a54
David Beck <david.beck.priv@gmail.com>
2016-04-17 16:59:41 +0200

    slight reorg

--- src/simple/mod.rs
+++ src/simple/mod.rs
@@ -1,7 +1,7 @@
 
 struct CircularBuffer<T : Copy> {
-  seqno : usize,
-  data  : Vec<T>,
+  seqno  : usize,
+  data   : Vec<T>,
 }
 
 struct CircularBufferIterator<'a, T: 'a + Copy> {
@@ -36,9 +36,9 @@ impl <T : Copy> CircularBuffer<T> {
 
   fn iter(&self) -> CircularBufferIterator<T> {
     CircularBufferIterator{
-      buffer: self,
-      position: self.min_pos(),
-      limit: self.seqno
+      buffer    : self,
+      position  : self.min_pos(),
+      limit     : self.seqno
      }
   }
 
@@ -83,64 +83,62 @@ impl <'_, T: '_ + Copy> Iterator for CircularBufferIterator<'_, T> {
 }
 
 pub fn tests() {
-  let mut x = CircularBuffer::new(4, 0 as i32);
+  let mut x = CircularBuffer::new(2, 0 as i32);
   x.put(|v| *v = 1);
   let mut y : i32 = 2;
-  x.put(|v| { *v = y; y += 1; });
-  x.put(|v| { *v = y; y += 1; });
   x.put(|v| { *v = y; y += 1; });
   x.put(|v| { *v = y; y += 1; });
 
   let it = x.iter();
   for i in it {
-    println!("B: {}", i);
+    println!("CB: {}", i);
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::CircularBuffer;
+
+  #[test]
+  #[should_panic]
+  fn create_zero_sized() {
+    let _x = CircularBuffer::new(0, 0 as i32);
   }
 
-  let count  = x.iter().count();
-  let max    = x.iter().max();
-  let first  = x.iter().take(1).last();
-  let last   = x.iter().last();
+  #[test]
+  fn empty_buffer() {
+    let x = CircularBuffer::new(1, 0 as i32);
+    let count = x.iter().count();
+    assert_eq!(count, 0);
+  }
 
-  println!("count={} max={} first={} last={}",
-    count,
-    max.unwrap(),
-    first.unwrap(),
-    last.unwrap());
-}
+  #[test]
+  fn overload_buffer() {
+    let mut x = CircularBuffer::new(2, 0 as i32);
+    x.put(|v| *v = 1);
+    x.put(|v| *v = 2);
+    x.put(|v| *v = 3);
+    assert_eq!(x.iter().count(), 2);
+    assert_eq!(x.iter().last().unwrap(), 3);
+    assert_eq!(x.iter().take(1).last().unwrap(), 2);
+  }
 
-#[test]
-#[should_panic]
-fn create_zero_sized() {
-  let _x = CircularBuffer::new(0, 0 as i32);
-}
-
-#[test]
-fn create_non_zero_sized() {
-  let _x = CircularBuffer::new(1, 0 as i32);
-}
-
-#[test]
-fn can_put() {
-  let mut x = CircularBuffer::new(1, 0 as i32);
-  x.put(|v| *v = 1);
-}
-
-#[test]
-fn can_put_with_env() {
-  let mut x = CircularBuffer::new(1, 0 as i32);
-  let mut y = 0;
-  let my_fn = |v : &mut i32| {
-    *v = y;
-    y += 1;
-  };
-  x.put(&my_fn);
-  x.put(&my_fn);
-  x.put(&my_fn);
-}
-
-#[test]
-fn can_put_with_env2() {
-  let mut x = CircularBuffer::new(1, 0 as i32);
-  let mut y = 0;
-  x.put(|v| { *v = y; y += 1; });
+  #[test]
+  fn can_put_with_env() {
+    let mut x = CircularBuffer::new(1, 0 as i32);
+    let mut y = 0;
+    {
+      let my_fn = |v : &mut i32| {
+        *v = y;
+        y += 1;
+      };
+      x.put(my_fn);
+    }
+    {
+      // the other way is
+      x.put(|v| { *v = y; y += 1; });
+      // TODO : check if I need this at all
+      //x.put(&my_fn);
+    }
+  }
 }
